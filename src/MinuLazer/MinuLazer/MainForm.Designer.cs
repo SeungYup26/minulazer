cf12395ef2bde25f5ec63152ea567797a0de7b94
seungyup26 <seungyup26@gmail.com>
2021-07-24 20:43:29 +0900

    Update MainForm.Designer.cs

--- src/MinuLazer/MinuLazer/MainForm.Designer.cs
+++ src/MinuLazer/MinuLazer/MainForm.Designer.cs
@@ -89,7 +89,7 @@ namespace MinuLazer
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(6, 5);
+            this.label1.Location = new System.Drawing.Point(6, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(159, 33);
             this.label1.TabIndex = 2;
